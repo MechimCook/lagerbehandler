c86d8040b9f1894c2b9311e1e83c1c747089c74c
Mechim <mechim@getlua.com>
2019-10-28 15:59:10 -0400

    Fix id not passing for delete

--- lib/lagerbehandler_web/live/user_view.ex
+++ lib/lagerbehandler_web/live/user_view.ex
@@ -16,7 +16,8 @@ defmodule LagerbehandlerWeb.Live.UserView do
     {:noreply, fetch(socket)}
   end
 
-  def handle_event("delete_user", id, socket) do
+  def handle_event("delete_user", value, socket) do
+    id = Map.get(value, "value")
     user = Lagerbehandler.UserManager.get_user(id)
     {:ok, _user} = Lagerbehandler.UserManager.delete_user(user)
 
